b05dc1dd5daebe9c7454d5ec2e74ab146960a4a2
Chris Wagner <eelhovercraft@gmail.com>
2012-07-30 20:56:51 -0400

    Asset updates

---
+++ js/scripts/town.js
@@ -0,0 +1,38 @@
+// you start by walking up a path with nothing but sand and the ruins of the old world.
+// you reach a large gate in a tall defensive wall at the end.
+
+message({
+    speaker: oldman,
+    text: [
+        "You... only those touched by the divine spark can open the gate to this town.",
+        "It's been over a hundred years since the last time this has happened...",
+        "I can only imagine your purpose. But I thank you for your presence."
+    ]
+});
+
+message({
+    speaker: woman,
+    text: [
+        "Mirv was the last person like you to show up.",
+        "She was very powerful, but she disappeared after she ventured into the tomb world."
+    ]
+});
+
+// find the entrance into the tomb world...
+
+message({
+    speaker: fakeMirv,
+    text: [
+        "I am Mirv. But those that enter the tomb world are changed forever.",
+        "You must be tired. Rest. Join me."
+        // fight it
+    ]
+})
+
+// walk up a staircase past the fakeMirv. real Mirv is encased in crystal. It resonates with your energy and she is freed.
+message({
+    speaker: mirv,
+    text: [
+        // ...
+    ]
+})
