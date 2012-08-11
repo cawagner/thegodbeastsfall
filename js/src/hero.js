7c6a2927d16ca25eb8de4f43419a7a380d8c9ef2
Chris Wagner <eelhovercraft@gmail.com>
2012-08-11 17:43:50 -0400

    Bit of cleanup

--- js/src/hero.js
+++ js/src/hero.js
@@ -1,49 +1,28 @@
-direction = {
-    UP: 0,
-    RIGHT: 1,
-    DOWN: 2,
-    LEFT: 3,
-    oppositeOf: function(d) {
-        switch(d) {
-            case direction.UP: return direction.DOWN;
-            case direction.LEFT: return direction.RIGHT;
-            case direction.RIGHT: return direction.LEFT;
-            case direction.DOWN: return direction.UP;
-        }
-    },
-    getFromXY: function(x, y) {
-        switch(true) {
-            case y > 0: return direction.DOWN;
-            case y < 0: return direction.UP;
-            case x < 0: return direction.LEFT;
-            case x > 0: return direction.RIGHT;
-            default: return direction.UP;
-        }
-    }
-};
-
 function Character(tilemap) {
-    var SPEED = 0.1;
-    var destX = 0, destY = 0,
-        moveX = 0, moveY = 0;
-    var moveRemaining = 0;
     var self = this;
+    var destX = 0, destY = 0, moveX = 0, moveY = 0;
+    var moveRemaining = 0;
+    
+
+    var resetMove = function() {
+        moveRemaining = 0;
+        self.x = destX;
+        self.y = destY;
+        moveX = 0;
+        moveY = 0;
+    };
 
     var moveTowardNewSquare = function() {
         if (self.canMove()) {
             return;
         }
 
-        self.x += SPEED * moveX;
-        self.y += SPEED * moveY;
+        self.x += Character.MOVE_SPEED * moveX;
+        self.y += Character.MOVE_SPEED * moveY;
 
-        moveRemaining -= SPEED;
+        moveRemaining -= Character.MOVE_SPEED;
         if (moveRemaining < 0) {
-            moveRemaining = 0;
-            self.x = destX;
-            self.y = destY;
-            moveX = 0;
-            moveY = 0;
+            resetMove();
         }
     };
 
@@ -70,11 +49,21 @@ function Character(tilemap) {
         return moveRemaining === 0;
     };
 
+    this.canMoveTo = function(x, y) {
+        return this.tilemap.isWalkable(x, y);
+    }
+
+    this.canMoveBy = function(dx, dy) {
+        return this.canMoveTo(this.x + dx, this.y + dy);
+    };
+
     this.x = 0;
     this.y = 0;
     this.direction = direction.UP;
     this.tilemap = tilemap;
 }
+
+Character.MOVE_SPEED = 0.1;
 
 Character.prototype.warpTo = function(x, y) {
     this.x = x;
@@ -85,7 +74,7 @@ Character.prototype.tryMoveBy = function(dx, dy) {
     // TODO: seems like it is doing too much...
     if (dx || dy) {
         this.direction = direction.getFromXY(dx, dy);
-        if (this.tilemap.isWalkable(this.x + dx, this.y + dy)) {
+        if (this.canMoveBy(dx, dy)) {
             this.moveBy(dx, dy);
         }
     }
@@ -95,9 +84,13 @@ Character.prototype.tryMoveBy = function(dx, dy) {
 function Hero(tilemap, input) {
     var self = this;
 
-    var takeWalkInput = function() {
+    var takeInput = function() {
         if (self.canMove()) {
-            self.tryMoveBy(input.dirX(), input.dirY());
+            if (self.tryMoveBy(input.dirX(), input.dirY())) {
+                // trigger step taken (check for random encounters, maybe update heros, etc)
+            } else {
+                // trigger "bump"
+            }
         }
     };
 
@@ -106,9 +99,14 @@ function Hero(tilemap, input) {
     this.update = (function(baseUpdate) {
         return function(timeScale) {
             baseUpdate.call(this, timeScale);
-            takeWalkInput();
+            takeInput();    
         };
     })(this.update);
+
+    this.addFollower = function(character) {
+
+    };
 };
 
 Hero.prototype = new Character();
+
