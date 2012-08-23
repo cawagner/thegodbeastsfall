29a29abed72fe4bc260388ddab5ce5d6cc927e25
Chris Wagner <eelhovercraft@gmail.com>
2012-08-22 23:37:22 -0400

    Starting to work out how skills will work

---
+++ js/src/skills.js
@@ -0,0 +1,43 @@
+function CharacterPawn(character) {
+    this.character = character;
+}
+
+CharacterPawn.prototype.attack = function() {
+    // TODO: factor in weapon / buffs!
+    return this.character.level + this.character.strength;
+};
+
+CharacterPawn.prototype.defense = function() {
+    // TODO: factor in armor / buffs!
+    return this.character.level + this.character.strength;
+};
+
+CharacterPawn.prototype.takeDamage = function() {
+
+};
+
+SkillEffects = {
+    "damage/melee": function(skill, user, targets) {
+        var results = [];
+
+        _(targets).each(function(target) {
+            var damage = (target.defense() / user.attack()) * skill.power;
+        });
+    }
+};
+
+Skills = {
+    "slash": {
+        "name": "Slash",
+        "target": "enemy", // enemy, enemies, ally, allies
+        "effect": "damage/melee",
+        "power": 10
+    },
+    "hard": {
+        "name": "Hard",
+        "target": "enemy",
+        "effect": "damage/melee",
+        "power": 20,
+        "cooldown": 1
+    }
+};
