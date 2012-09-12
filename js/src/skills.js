28f9ba12be2bccbf81405c7ca73e107d2d8dbdb7
Chris Wagner <Chris.Wagner@telerik.com>
2012-09-11 23:53:40 -0400

    Formulae tweaks, can great critical

--- js/src/skills.js
+++ js/src/skills.js
@@ -7,11 +7,18 @@ SkillEffects = (function() {
                 dice = Dice.parse(skill.power);
 
             _(targets).each(function(target) {
-                var damage = (target.defense() / user.attack()) * dice.value(),
+                var ratio = Math.max(0.1, 1 + (2*user.attack() - target.defense()) / 20);
+                var damage = ratio * dice.value(),
                     hitChance = (skill.accuracy * user.accuracy() - target.evade()),
-                    criticalChance = Math.max(2, user.criticalChance() * skill.criticalChance - target.luck());
+                    criticalChance = Math.max(2, 1 + user.criticalChance() * skill.criticalChance - target.luck());
                 var isCritical = d100.roll() <= criticalChance;
+                var isGreatCritical = isCritical && (d100.roll() <= criticalChance);
                 var hasConnected = isCritical || (d100.roll() <= hitChance);
+
+                if (isGreatCritical) {
+                    // Great critical assumes full dice rolls, and ignores armor value that would reduce attack strength below 100%
+                    damage = Math.max(1, ratio) * dice.max();
+                }
 
                 if (isCritical) {
                     damage = damage * skill.criticalMultipler * user.criticalMultiplier();
@@ -23,6 +30,7 @@ SkillEffects = (function() {
                     success: true,
                     missed: !hasConnected,
                     critical: isCritical,
+                    greatCritical: isGreatCritical,
                     damage: hasConnected ? damage : 0,
                     target: target
                 });
