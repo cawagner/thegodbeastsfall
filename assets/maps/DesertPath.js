e221a3bc77c831c7a793304343f38a59c10c6f5b
Chris Wagner <Chris.Wagner@telerik.com>
2012-09-11 20:11:21 -0400

    Got tired of god-beast Ropo's yammerings

--- assets/maps/DesertPath.js
+++ assets/maps/DesertPath.js
@@ -1,14 +1,7 @@
 setupMap(function(map) {
     map.onLoad = function(hero, entrance) {
         if (entrance === "default") {
-            var messages = [
-                {
-                    text: [
-                        "Seo ii si Ropo nani ma seln au ansiefta sen Held bnani.",
-                    ]
-                }
-            ];
-            new Actor().say(messages);
+            // derp derp
         }
     };
 });
