078256037a458083a975c16696aff9bad7a9bd52
Chris Wagner <eelhovercraft@gmail.com>
2012-10-01 00:13:21 -0400

    New touch input

--- js/src/touch-input.js
+++ js/src/touch-input.js
@@ -7,6 +7,63 @@ function initTouchInput() {
         return false;
     });
 
+    var input = Game.instance.input;
+
+    var keyCodesToKeys = {
+        37: 'left',
+        39: 'right',
+        38: 'up',
+        40: 'down',
+        90: 'confirm',
+        88: 'cancel'
+    };
+
+    var movement = function(touches) {
+        var pos = dpad.position();
+        var x = touches[0].pageX - pos.left;
+        var y = touches[0].pageY - pos.top + 60;
+        var size = 60;
+
+        var dirs = [
+            { keyCode: 37, fn: "isLeftDown", predicate: function() { return x < size; } },
+            { keyCode: 39, fn: "isRightDown", predicate: function() { return x > 2*size; } },
+            { keyCode: 38, fn: "isUpDown", predicate: function() { return y < size; } },
+            { keyCode: 40, fn: "isDownDown", predicate: function() { return y > 2*size; } }
+        ];
+
+        _(dirs).each(function(dir){
+            var isKeyDown = input[dir.fn]();
+            if (dir.predicate()) {
+                if (!isKeyDown) {
+                    document.onkeydown(dir);
+                }
+            } else {
+                if (isKeyDown) {
+                    document.onkeyup(dir);
+                }
+            }
+        });
+    };
+
+    var dpad = $(".dpad").on("touchstart", function(e) {
+        movement(e.originalEvent.touches);
+    }).on("touchmove", function(e) {
+        movement(e.originalEvent.touches);
+    }).on("touchend", function(e) {
+        if (input.isLeftDown()) {
+            document.onkeyup({ keyCode: 37 });
+        }
+        if (input.isRightDown()) {
+            document.onkeyup({ keyCode: 39 });
+        }
+        if (input.isUpDown()) {
+            document.onkeyup({ keyCode: 38 });
+        }
+        if (input.isDownDown()) {
+            document.onkeyup({ keyCode: 40 });
+        }
+    });
+
     $("#touchControls").change(function(){
         $("body").toggleClass("touchControls", $(this).is(":checked"));
     });
