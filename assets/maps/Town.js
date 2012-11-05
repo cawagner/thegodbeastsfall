1b0999ed2a69c3305cf1d5096baa1da2aa77706c
Chris Wagner <eelhovercraft@gmail.com>
2012-11-04 22:15:13 -0500

    Oh man, so much less code for the town...

--- assets/maps/Town.js
+++ assets/maps/Town.js
@@ -1,62 +1,33 @@
 setupMap(function(map, gameState) {
     "use strict";
 
-    var npcLearn = 400;
-
+    var stepsUntilMirvMessage = 750;
     var learnAboutMirv = function() {
-        gameState.flags.town.stepsUntilMirvMessage = Math.max(
-            5,
-            gameState.flags.town.stepsUntilMirvMessage - npcLearn
-        );
-        npcLearn /= 2;
+        stepsUntilMirvMessage -= 250;
+        if (stepsUntilMirvMessage < 5) {
+            stepsUntilMirvMessage = 5;
+        }
     };
 
-    gameState.flags.town = gameState.flags.town || {
-        knowAboutMirv: false,
-        stepsUntilMirvMessage: 750
+    var flags = gameState.flags.town = gameState.flags.town || {
+        knowAboutMirv: false
     };
 
     map.npcs.oldman.addBeforeTalk(learnAboutMirv);
-
-    map.npcs.littlegirl.onTalk = function() {
-        var message =  {
-            speaker: "littlegirl",
-            text: [
-                "Mirv is really pretty. And really strong. And smart.",
-                "I hope she's okay."
-            ]
-        };
-        this.say([message]);
-        learnAboutMirv();
-    };
-
-    map.npcs.earl.onTalk = function() {
-        var message = {
-            speaker: "earl",
-            text: [
-                "You're the first person like you to come here in over twenty years.",
-                "Mirv was the last, but she went into the tomb world and never came back."
-            ]
-        };
-        this.say([message]);
-        learnAboutMirv();
-    };
+    map.npcs.littlegirl.addBeforeTalk(learnAboutMirv);
+    map.npcs.earl.addBeforeTalk(learnAboutMirv);
 
     map.npcs.barrel1.onTalk = function() {
-        var message = {
-            text: [
-                "This barrel is full of nothing but hatred and lies."
-            ]
-        };
-        this.say([message]);
+        // we'll flesh this out over time...
+        $.publish("/battle/start");
     };
 
     $.subscribe("/hero/step", function() {
-        if (gameState.flags.town.knowAboutMirv)
+        if (flags.knowAboutMirv)
             return;
 
-        gameState.flags.town.stepsUntilMirvMessage--;
-        if (gameState.flags.town.stepsUntilMirvMessage < 0) {
+        stepsUntilMirvMessage--;
+        if (stepsUntilMirvMessage < 0) {
             $.publish("/npc/talk", [[{
                 text: [
                     "Held... can you hear me? I am Mirv. It is preordained that we work together...",
@@ -66,7 +37,7 @@ setupMap(function(map, gameState) {
                     "Please, if you can hear me..."
                 ]
             }]]);
-            gameState.flags.town.knowAboutMirv = true;
+            flags.knowAboutMirv = true;
         }
     });
 });
