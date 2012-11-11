5e449621da8ec3fc13abf75d8508c031667650bf
Chris Wagner <eelhovercraft@gmail.com>
2012-11-11 16:05:42 -0500

    Move battle menu to own file

--- js/src/states/battle-state.js
+++ js/src/states/battle-state.js
@@ -3,13 +3,13 @@ define([
     "game-state",
     "pawns/pawns",
     "gui",
-    "menu"
+    "battle/battle-menu"
 ], function(
     _,
     gameState,
     pawns,
     GuiRenderer,
-    Menu
+    battleMenu
 ) {
     var MESSAGE_DELAY = 250;
 
@@ -38,43 +38,6 @@ define([
         //     var menu = self.getMenu();
         //     menu.open();
         // }, 2000);
-    };
-
-    BattleState.prototype.skillsOfType = function(type) {
-        var self = this;
-        return function() {
-            var member = self.playerPawns[self.partyIndex];
-            var skills = member.character.skills[type];
-            return new Menu({
-                items: skills
-            });
-        };
-    };
-
-    BattleState.prototype.getMenu = function() {
-        return new Menu({
-            hierarchical: true,
-            rows: 2,
-            cols: 2,
-            x: 10,
-            y: 200,
-            items: [
-                { text: "Fight", childMenu: this.skillsOfType("Fight") },
-                { text: "Magic", childMenu: this.skillsOfType("Magic") },
-                { text: "Item", childMenu: new Menu() },
-                {
-                    text: "Tactic",
-                    childMenu: new Menu({
-                        items: [ "Defend", "Run Away", "Inspect" ],
-                        rows: 1,
-                        cols: 3,
-                        x: 10,
-                        y: 200
-                    })
-                }
-            ],
-            cancel: _.give(false)
-        });
     };
 
     BattleState.prototype.messagePhase = function() {
@@ -107,7 +70,7 @@ define([
     };
 
     BattleState.prototype.enterCommands = function() {
-        this.getMenu().open();
+        battleMenu.get(this).open();
         this.enqueueState(_.noop);
         this.advanceState();
     };
