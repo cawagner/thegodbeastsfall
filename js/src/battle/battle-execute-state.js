0a718d4a54c8f0733786378ec573ab6977aff51b
Chris Wagner <eelhovercraft@gmail.com>
2012-11-22 14:05:01 -0500

    Losing fights now recognized, even if the result is stupid

--- js/src/battle/battle-execute-state.js
+++ js/src/battle/battle-execute-state.js
@@ -6,6 +6,8 @@ define([
 ], function(_, $, BattleMessageState, BattleWonState) {
     "use strict";
 
+    // TODO: this whole file is a mess... lol 3:00AM
+
     // TODO: move
     function BuryTheDeadState(pawn) {
         this.start = function() {
@@ -57,7 +59,67 @@ define([
     };
 
     return function BattleExecuteState(battleState, actions, nextRound) {
+
+        var wonBattle = function() {
+            return _(battleState.enemyPawns).all(function(pawn) { return !pawn.isAlive(); });
+        };
+
+        var lostBattle = function() {
+            return _(battleState.playerPawns).all(function(pawn) { return !pawn.isAlive(); });
+        };
+
+        var xpPerPerson = function() {
+            var totalXp = 0;
+            _(battleState.enemyPawns).each(function(pawn) {
+                totalXp += pawn.xp();
+            });
+            return Math.ceil(totalXp / battleState.playerPawns.length);
+        };
+
+        var playMusic = function(name) {
+            battleState.enqueueState({
+                start: function() { $.publish("/music/play", [name]); },
+                update: _.give(true),
+                draw: _.noop
+            });
+        };
+
+        var winBattle = function() {
+            var xp = xpPerPerson();
+
+            playMusic("victory");
+
+            battleState.enqueueState(new BattleMessageState([
+                "All monsters perished!",
+                "Got " + xp + "XP each!"
+            ]));
+            battleState.enqueueState(new BattleWonState(xp));
+        };
+
+        var loseBattle = function() {
+            playMusic("defeat");
+
+            battleState.enqueueState(new BattleMessageState([
+                "It looks like you got your head handed to you.",
+                "How regrettable."
+            ]));
+
+            battleState.enqueueState({
+                start: function() {
+                    // TODO: do something besides just reviving players and ending the battle...
+                    _(battleState.playerPawns).each(function(player) {
+                        player.restoreHp(1);
+                    });
+                    $.publish("/battle/end");
+                },
+                draw: _.noop,
+                update: _.noop
+            });
+        }
+
         this.start = function() {
+            var xp;
+
             console.log(actions);
             _(actions).each(function(action) {
                 executeUseSkillAction(action, battleState);
@@ -70,26 +132,10 @@ define([
                 enemy.refresh();
             });
 
-            if (_(battleState.enemyPawns).all(function(pawn) { return !pawn.isAlive(); })) {
-                var totalXp = 0;
-                _(battleState.enemyPawns).each(function(pawn) {
-                    totalXp += pawn.xp();
-                });
-                var xpPerPerson = Math.ceil(totalXp / battleState.playerPawns.length);
-
-                battleState.enqueueState({
-                    start: function() {
-                        $.publish("/music/play", ["victory"]);
-                    },
-                    update: _.give(true),
-                    draw: _.noop
-                });
-
-                battleState.enqueueState(new BattleMessageState([
-                    "All monsters perished!",
-                    "Got " + xpPerPerson + "XP each!"
-                ]));
-                battleState.enqueueState(new BattleWonState(xpPerPerson));
+            if (wonBattle()) {
+                winBattle();
+            } else if (lostBattle()) {
+                loseBattle();
             } else {
                 nextRound();
             }
--- js/src/pawns/character-pawn.js
+++ js/src/pawns/character-pawn.js
@@ -9,12 +9,16 @@ define(["pawns/pawn-base"], function(PawnBase) {
     CharacterPawn.prototype = new PawnBase();
 
     CharacterPawn.prototype.takeDamage = function(amount) {
-        this.character.hp -= amount;
+        this.character.hp = Math.max(0, this.character.hp - amount);
     };
 
     CharacterPawn.prototype.consumeMp = function(amount) {
         this.character.mp -= amount;
-    }
+    };
+
+    CharacterPawn.prototype.restoreHp = function(amount) {
+        this.character.hp = Math.min(this.character.maxHp, this.character.hp + amount);
+    };
 
     return CharacterPawn;
 });
