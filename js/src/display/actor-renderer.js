21b338a4205fee7fdc2dc137bc94f2c873f9b8ca
Chris Wagner <Chris.Wagner@telerik.com>
2012-11-26 20:01:58 -0500

    Only load each archetype image once; support start frames

--- js/src/display/actor-renderer.js
+++ js/src/display/actor-renderer.js
@@ -2,11 +2,14 @@ define(["json!archetypes.json", "image-loader", "constants"], function(archetype
     "use strict";
 
     // TODO: doesn't really live here...
+    var archetypeImages = {};
     _(archetypes).each(function(archetype, key) {
         if (archetype.imagePath !== undefined) {
-            archetype.image = imageLoader.loadImageSync(archetype.imagePath);
+            archetypeImages[archetype.imagePath] = imageLoader.loadImageSync(archetype.imagePath);
         }
     });
+
+    console.log(archetypeImages);
 
     var ActorRenderer = function(graphics) {
         var walkFrames = [1,0,1,2];
@@ -14,19 +17,23 @@ define(["json!archetypes.json", "image-loader", "constants"], function(archetype
         var destRect = { x: 0, y: 0, width: 16, height: 18 };
 
         this.drawActor = function(actor, frame) {
-            var frameToDraw, srcRect, archetype = archetypes[actor.archetype];
+            var frameToDraw,
+                srcRect,
+                archetype = archetypes[actor.archetype],
+                image = archetypeImages[archetype.imagePath];
+            console.log(image);
 
             if (archetype.isHidden)
                 return;
 
             frameToDraw = actor.direction * 3 + walkFrames[Math.floor(frame)];
 
-            srcRect = graphics.getRectForFrame(frameToDraw, archetype.image.width, 16, 18);
+            srcRect = graphics.getRectForFrame(frameToDraw + (archetype.startFrame || 0), image.width, 16, 18);
 
             destRect.x = actor.x * constants.TILE_SIZE;
             destRect.y = actor.y * constants.TILE_SIZE - 4;
 
-            graphics.drawImageRect(archetype.image, srcRect, destRect);
+            graphics.drawImageRect(image, srcRect, destRect);
         };
     };
 
