dfd271fd847aad629f900d5fbec9ddcde8c41ab7
Chris Wagner <Chris.Wagner@telerik.com>
2013-09-16 23:52:48 -0400

    Not pretending to be OO when we aren't

--- src/js/src/keyboard-input.js
+++ src/js/src/keyboard-input.js
@@ -7,85 +7,77 @@ define([], function() {
         };
     };
 
-    var KeyboardInput = function() {
-        var keys = {
-            left: false,
-            right: false,
-            up: false,
-            down: false,
-            confirm: false,
-            cancel: false
+    var keys = {
+        left: false,
+        right: false,
+        up: false,
+        down: false,
+        confirm: false,
+        cancel: false
+    };
+
+    var pressed = { };
+
+    var keyCodesToKeys = {
+        37: 'left',
+        39: 'right',
+        38: 'up',
+        40: 'down',
+        90: 'confirm',
+        88: 'cancel'
+    };
+
+    var setKeyTo = function(state) {
+        return function(e) {
+            if (keyCodesToKeys[e.keyCode]) {
+                keys[keyCodesToKeys[e.keyCode]] = state;
+                pressed[keyCodesToKeys[e.keyCode]] = state;
+            }
         };
-        var pressed = { };
+    };
 
-        var isConfirmDown;
-
-        var keyCodesToKeys = {
-            37: 'left',
-            39: 'right',
-            38: 'up',
-            40: 'down',
-            90: 'confirm',
-            88: 'cancel'
+    var wasPressed = function(key) {
+        return function() {
+            var result = pressed[key];
+            pressed[key] = false;
+            return result;
         };
+    };
 
-        var setKeyTo = function(state) {
-            return function(e) {
-                if (keyCodesToKeys[e.keyCode]) {
-                    keys[keyCodesToKeys[e.keyCode]] = state;
-                    pressed[keyCodesToKeys[e.keyCode]] = state;
-                }
-            };
-        };
+    var keys;
+    return {
+        isLeftDown: getter(keys, 'left'),
+        isRightDown: getter(keys, 'right'),
+        isUpDown: getter(keys, 'up'),
+        isDownDown: getter(keys, 'down'),
+        isConfirmDown: getter(keys, 'confirm'),
+        isCancelDown: getter(keys, 'cancel'),
 
-        var wasPressed = function(key) {
-            return function() {
-                var result = pressed[key];
-                pressed[key] = false;
-                return result;
-            };
-        };
+        wasConfirmPressed: wasPressed('confirm'),
+        wasCancelPressed: wasPressed('cancel'),
 
-        this.onKeyDown = setKeyTo(true);
-        this.onKeyUp = setKeyTo(false);
+        wasUpPressed: wasPressed('up'),
+        wasDownPressed: wasPressed('down'),
+        wasLeftPressed: wasPressed('left'),
+        wasRightPressed: wasPressed('right'),
 
-        this.isLeftDown = getter(keys, 'left');
-        this.isRightDown = getter(keys, 'right');
-        this.isUpDown = getter(keys, 'up');
-        this.isDownDown = getter(keys, 'down');
-        this.isConfirmDown = getter(keys, 'confirm');
-        this.isCancelDown = getter(keys, 'cancel');
-
-        this.wasConfirmPressed = wasPressed('confirm');
-        this.wasCancelPressed = wasPressed('cancel');
-
-        this.wasUpPressed = wasPressed('up');
-        this.wasDownPressed = wasPressed('down');
-        this.wasLeftPressed = wasPressed('left');
-        this.wasRightPressed = wasPressed('right');
-
-        this.init = function() {
-            document.onkeydown = this.onKeyDown;
-            document.onkeyup = this.onKeyUp;
-        };
-
-        this.dirX = function() {
+        init: function() {
+            document.onkeydown = setKeyTo(true);
+            document.onkeyup = setKeyTo(false);
+        },
+        dirX: function() {
             return this.isRightDown() - this.isLeftDown();
-        };
-
-        this.dirY = function() {
+        },
+        dirY: function() {
             return this.isDownDown() - this.isUpDown();
-        };
-
-        this.anyInput = function() {
+        },
+        anyInput: function() {
             // TODO: use lazy.
             for (var key in keys) {
                 if (keys[key])
                     return true;
             }
             return false;
-        };
+        }
     };
-
-    return new KeyboardInput();
 });
