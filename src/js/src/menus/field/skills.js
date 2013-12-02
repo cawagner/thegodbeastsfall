75c5fe72c7751dcf2f334866f373436c9b607d3d
Chris Wagner <eelhovercraft@gmail.com>
2013-12-02 18:32:10 -0500

    Slight cleanup

--- src/js/src/menus/field/skills.js
+++ src/js/src/menus/field/skills.js
@@ -21,6 +21,37 @@ define([
 ) {
     "use strict";
 
+    var createEffectExecutor = function(action, messages) {
+        return new EffectExecutor({
+            state: new CompositeState(),
+            action: action,
+            displayMessage: function(msg) {
+                // HACK: ugh, gross
+                return {
+                    start: function() { messages.push(msg); },
+                    update: _.give(true),
+                    draw: _.noop
+                };
+            },
+            displayDamage: function(target, amount) {
+                // HACK: ugh, gross AND don't hardcode text
+                return {
+                    start: function() {
+                        if (amount === 0) {
+                            return;
+                        } else if (amount > 0) {
+                            messages.push(target.name + " recovered " + amount + " hit points!");
+                        } else if (amount < 0) {
+                            messages.push(target.name + " took " + amount + " damage!");
+                        }
+                    },
+                    update: _.give(true),
+                    draw: _.noop
+                };
+            }
+        });
+    };
+
     var createSkillMenuForMember = function(member) {
         var pawn = new CharacterPawn(member);
         var fieldSkills = _(member.skills["Magic"]).chain().filter(function(skill) {
@@ -41,52 +72,26 @@ define([
                     ? pawn
                     : new CharacterPawn(gameState.party[index]);
                 var messages = [];
-                var executor = new EffectExecutor({
-                    state: new CompositeState(),
-                    action: {
-                        user: pawn,
-                        skill: fieldSkill.skill,
-                        targets: [targetPawn]
-                    },
-                    displayMessage: function(msg) {
-                        // HACK: ugh, gross
-                        return {
-                            start: function() { messages.push(msg); },
-                            update: _.give(true),
-                            draw: _.noop
-                        };
-                    },
-                    displayDamage: function(target, amount) {
-                        // HACK: ugh, gross AND don't hardcode text
-                        return {
-                            start: function() {
-                                if (amount === 0) {
-                                    return;
-                                } else if (amount > 0) {
-                                    messages.push(target.name + " recovered " + amount + " hit points!");
-                                } else if (amount < 0) {
-                                    messages.push(target.name + " took " + amount + " damage!");
-                                }
-                            },
-                            update: _.give(true),
-                            draw: _.noop
-                        };
-                    }
-                });
+                var executor = createEffectExecutor({
+                    user: pawn,
+                    skill: fieldSkill.skill,
+                    targets: [targetPawn]
+                }, messages);
                 skillUser.useSkill(executor);
                 executor.state.runAll();
                 radio("/npc/talk").broadcast({
                     text: messages
                 });
             };
+            var skillIsUsable = function() {
+                return !pawn.canUseSkill(fieldSkill.skill);
+            };
             var targetMenu = new Menu({
-                items: _(gameState.party).pluck("name").map(function(memberName) {
+                items: _(gameState.party).map(function(member) {
                     return {
-                        text: memberName,
-                        disabled: function() {
-                            return !pawn.canUseSkill(fieldSkill.skill);
-                        }
-                    }
+                        text: member.name,
+                        disabled: skillIsUsable
+                    };
                 }),
                 select: targetPartyMenuSelected
             }).open();
